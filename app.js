5adde68b7d0408282fd205475afa80313078f844
javierFernandezGlez <javierfer900610@gmail.com>
2022-09-07 17:23:49 -0400

    Finished, Bonus Iteration 6 not done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
+hbs.registerPartials(__dirname + '/views/partials')
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
@@ -21,5 +21,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+
+app.get('/beers', (request, response) => {
+  punkAPI.getBeers()
+  .then(beersFromAPI => {
+    console.log(beersFromAPI);
+    let data = {
+      beers:[]
+    };
+    // let data = {
+    //   name:[],
+    //   description:[],
+    //   images: [],
+    //   tagline: []
+    // }
+    for(let beer of beersFromAPI) {
+      let object = {}
+      object.image = beer.image_url;
+      object.description = beer.description;
+      object.name = beer.name;
+      object.tagline = beer.tagline;
+      data.beers.push(object);
+    }
+    response.render('beers', data);
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (request, response) => {
+  punkAPI.getRandom()
+    .then(responseFromAPI => {
+      response.render("random-beer", responseFromAPI[0]);
+    })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
